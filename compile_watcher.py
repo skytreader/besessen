5d5d8846fffbac0ea6c5a2c0dd516b1bd53f66d0
Chad Estioco <chadestioco@gmail.com>
2020-12-02 07:49:53 +0800

    Factor out TS stuff.

--- compile_watcher.py
+++ compile_watcher.py
@@ -3,6 +3,7 @@ import time
 import logging
 import os
 import subprocess
+from abc import ABC
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, LoggingEventHandler
 
@@ -11,11 +12,12 @@ This script watches all Typescript files in the current directory and compiles
 them if and when they change.
 """
 
-class CompileEventHandler(FileSystemEventHandler):
+class CompileEventHandler(FileSystemEventHandler, ABC):
 
-    def __init__(self, build_dir=None):
+    def __init__(self, build_dir=None, file_exts=()):
         super(CompileEventHandler, self).__init__()
         self.build_dir = build_dir[0:-1] if build_dir and build_dir[-1] == "/" else build_dir
+        self.extensions = ((x if x.startswith(".") else "." + x) for x in file_exts)
 
         if os.path.exists(self.build_dir):
             if not os.path.isdir(self.build_dir):
@@ -30,29 +32,19 @@ class CompileEventHandler(FileSystemEventHandler):
                         continue
 
                 for name in filenames:
-                    if name.endswith(".ts"):
+                    if self.__should_observe(name):
                         fullfilename = os.path.join(dirpath, name)
-                        self.__compile(fullfilename)
-            
-    
-    def __to_js(self, filename):
-        """
-        Assumes that you give it ".ts" files and changes the extension to ".js".
-        """
-        filename_parse = filename.split(".")
-        sans_extension = ".".join(filename_parse[0:-1])
-        if self.build_dir:
-            sans_extension_parse = sans_extension.split("/")
-            sans_extension_parse[-2] = self.build_dir
-            sans_extension = "/".join(sans_extension_parse)
-        return sans_extension + ".js"
-    
-    def __compile(self, src):
-        outfile = self.__to_js(src)
-        subprocess.call([
-            "node_modules/typescript/bin/tsc", "--lib", "es2015,es2015.iterable,dom", "--outFile", outfile, src
-        ])
-        logging.info("compiled %s to %s" % (src, outfile))
+                        self.compile(fullfilename)
+
+    def __should_observe(self, fname):
+        for extension in self.extensions:
+            if fname.endswith(extension):
+                return True
+        return False
+
+    @abstractmethod
+    def compile(self, src):
+        pass
     
     def __delete(self, fname):
         outfile = self.__to_js(fname)
@@ -60,7 +52,7 @@ class CompileEventHandler(FileSystemEventHandler):
         logging.info("deleted %s" % outfile)
 
     def __is_ts_event(self, event):
-        return not event.is_directory and event.src_path.endswith(".ts")
+        return not event.is_directory and self.__should_observe(event.src_path)
 
     def on_created(self, event):
         super(CompileEventHandler, self).on_created(event)
@@ -89,8 +81,32 @@ class CompileEventHandler(FileSystemEventHandler):
         if self.__is_ts_event(event):
             logging.debug("TS moved event %s" % event)
             self.__delete(event.src_path)
-            if event.dest_path.endswith(".ts"):
+            if self.__should_observe(event.dest_path):
                 self.__compile(event.dest_path)
+
+class TSCompiler(CompileEventHandler):
+
+    def __init__(sefl, build_dir):
+        super().__init__(build_dir, ("ts",))
+    
+    def __to_js(self, filename):
+        """
+        Assumes that you give it ".ts" files and changes the extension to ".js".
+        """
+        filename_parse = filename.split(".")
+        sans_extension = ".".join(filename_parse[0:-1])
+        if self.build_dir:
+            sans_extension_parse = sans_extension.split("/")
+            sans_extension_parse[-2] = self.build_dir
+            sans_extension = "/".join(sans_extension_parse)
+        return sans_extension + ".js"
+    
+    def compile(self, src):
+        outfile = self.__to_js(src)
+        subprocess.call([
+            "node_modules/typescript/bin/tsc", "--lib", "es2015,es2015.iterable,dom", "--outFile", outfile, src
+        ])
+        logging.info("compiled %s to %s" % (src, outfile))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
